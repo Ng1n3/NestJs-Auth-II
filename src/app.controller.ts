129678c01a0f04148cd19fe4ff89556629303b98
ng1n3 <code.with.muyiwa@gmail.com>
2024-01-23 12:27:59 +0100

    postgres database setup and config with docker and prisma

--- src/app.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
-import { LocalAuthGuard } from './auth/local-auth.guard';
-import { AuthService } from './auth/auth.service';
-
-@Controller()
-export class AppController {
-
-}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthService } from './auth/auth.service';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [],
 })
 export class AppModule {}
