10706a220a7fdd02b012ae55a63b30c1a35d2c3f
ng1n3 <code.with.muyiwa@gmail.com>
2024-01-23 11:25:19 +0100

    restarting the project

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,8 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './auth/local-auth.guard';
-import { AppService } from './app.service';
-import { AuthenticatedGuard } from './auth/authenticated.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
-  @UseGuards(LocalAuthGuard)
-  @Post('login')
-  login(@Request() req: any): any {
-    return {msg: 'Logged in!'};
-  }
-
-  @UseGuards(AuthenticatedGuard)
-  @Get('protected')
-  getHello(@Request() req: any): String {
-    return req.user
-  }
 }
